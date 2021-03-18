18146414447eb4e387fbc479f591979018edc3ab
hecto <hecto@LENOVO>
2021-03-18 16:39:18 -0600

    Agregar archivos de proyecto.

---
+++ Model/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiWeb.Model
+{
+    public class TodoContext
+    {
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Todoitem> TodoItems { get; set; }
+
+    }
+}
